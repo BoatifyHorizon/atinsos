7d1fd9ff643d770a2019e035a808572650bf1060
Jakub <pietrusjakub@gmail.com>
2025-01-26 12:57:42 +0100

    2 first commit

---
+++ src/main/java/com/wsis/atinsos/AtinsosApplication.java
@@ -0,0 +1,13 @@
+package com.wsis.atinsos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtinsosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtinsosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wsis/atinsos/AtinsosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wsis.atinsos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtinsosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
