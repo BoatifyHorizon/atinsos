50a223ecaa4db2df622af2c99868e84eac315260
kplusa <kacper.plusaa@gmail.com>
2025-01-29 06:42:53 +0100

    Add entity models - lab 7

---
+++ src/main/java/com/wsis/atinsos/model/Rezerwacja.java
@@ -0,0 +1,66 @@
+package com.wsis.atinsos.model;
+
+import jakarta.persistence.*;
+import java.sql.Date;
+
+@Entity
+@Table(name = "REZERWACJA")
+public class Rezerwacja {
+
+    @Id
+    @Column(name = "ID")
+    private Integer id;
+
+    @Column(name = "DATA_REZERWACJI")
+    private Date dataRezerwacji;
+
+    @Column(name = "STATUS_REZERWACJI", length = 50)
+    private String statusRezerwacji;
+
+    @ManyToOne
+    @JoinColumn(name = "UZYTKOWNIK_ID")
+    private Uzytkownik uzytkownik;
+
+    @ManyToOne
+    @JoinColumn(name = "POZYCJA_BIBLIOTECZNA_ID")
+    private PozycjaBiblioteczna pozycjaBiblioteczna;
+
+    public Rezerwacja() {
+    }
+
+    // Getters & setters
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Date getDataRezerwacji() {
+        return dataRezerwacji;
+    }
+    public void setDataRezerwacji(Date dataRezerwacji) {
+        this.dataRezerwacji = dataRezerwacji;
+    }
+
+    public String getStatusRezerwacji() {
+        return statusRezerwacji;
+    }
+    public void setStatusRezerwacji(String statusRezerwacji) {
+        this.statusRezerwacji = statusRezerwacji;
+    }
+
+    public Uzytkownik getUzytkownik() {
+        return uzytkownik;
+    }
+    public void setUzytkownik(Uzytkownik uzytkownik) {
+        this.uzytkownik = uzytkownik;
+    }
+
+    public PozycjaBiblioteczna getPozycjaBiblioteczna() {
+        return pozycjaBiblioteczna;
+    }
+    public void setPozycjaBiblioteczna(PozycjaBiblioteczna pozycjaBiblioteczna) {
+        this.pozycjaBiblioteczna = pozycjaBiblioteczna;
+    }
+}
---
+++ src/main/java/com/wsis/atinsos/model/Wypozyczenie.java
@@ -0,0 +1,76 @@
+package com.wsis.atinsos.model;
+
+import jakarta.persistence.*;
+import java.sql.Date;
+
+@Entity
+@Table(name = "WYPOZYCZENIE")
+public class Wypozyczenie {
+
+    @Id
+    @Column(name = "ID")
+    private Integer id;
+
+    @Column(name = "DATA_WYPOZYCZENIA")
+    private Date dataWypozyczenia;
+
+    @Column(name = "TERMIN_ZWROTU")
+    private Date terminZwrotu;
+
+    @Column(name = "DATA_ZWROTU")
+    private Date dataZwrotu;
+
+    @ManyToOne
+    @JoinColumn(name = "UZYTKOWNIK_ID")
+    private Uzytkownik uzytkownik;
+
+    @ManyToOne
+    @JoinColumn(name = "POZYCJA_BIBLIOTECZNA_ID")
+    private PozycjaBiblioteczna pozycjaBiblioteczna;
+
+    public Wypozyczenie() {
+    }
+
+    // Getters & setters
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Date getDataWypozyczenia() {
+        return dataWypozyczenia;
+    }
+    public void setDataWypozyczenia(Date dataWypozyczenia) {
+        this.dataWypozyczenia = dataWypozyczenia;
+    }
+
+    public Date getTerminZwrotu() {
+        return terminZwrotu;
+    }
+    public void setTerminZwrotu(Date terminZwrotu) {
+        this.terminZwrotu = terminZwrotu;
+    }
+
+    public Date getDataZwrotu() {
+        return dataZwrotu;
+    }
+    public void setDataZwrotu(Date dataZwrotu) {
+        this.dataZwrotu = dataZwrotu;
+    }
+
+    public Uzytkownik getUzytkownik() {
+        return uzytkownik;
+    }
+    public void setUzytkownik(Uzytkownik uzytkownik) {
+        this.uzytkownik = uzytkownik;
+    }
+
+    public PozycjaBiblioteczna getPozycjaBiblioteczna() {
+        return pozycjaBiblioteczna;
+    }
+    public void setPozycjaBiblioteczna(PozycjaBiblioteczna pozycjaBiblioteczna) {
+        this.pozycjaBiblioteczna = pozycjaBiblioteczna;
+    }
+}
