646591ade7a75cfebacc27c60c403ec452d3a5ba
Kacper Płusa <kacper.plusaa@gmail.com>
2025-02-10 18:34:15 +0100

    👌 Apply cr suggestions

--- src/test/java/com/wsis/atinsos/WydzialDaoImplTest.java
+++ src/test/java/com/wsis/atinsos/WydzialDaoImplTest.java
@@ -29,21 +29,21 @@ class WydzialDaoImplTest {
     }
 
     @Test
-    void testSave() {
+    void shouldPersistEntity_WhenValidWydzialIsSaved() {
         var name = "Wydzial A";
-    
+
         doNothing().when(entityManager).persist(wydzial);
-    
+
         wydzialDao.save(wydzial);
-    
+
         verify(entityManager, times(1)).persist(wydzial);
-    
+
         assertNotNull(wydzial.getId());
         assertEquals(name, wydzial.getNazwa());
     }
-    
+
     @Test
-    void testFindById() {
+    void shouldReturnWydzial_WhenExistingIdIsProvided() {
         var id = 1;
         when(entityManager.find(Wydzial.class, id)).thenReturn(wydzial);
 
@@ -54,13 +54,13 @@ class WydzialDaoImplTest {
     }
 
     @Test
-    void testUpdate() {
+    void shouldMergeEntity_WhenValidWydzialIsUpdated() {
         var updatedName = "Updated Wydzial";
         wydzial.setNazwa(updatedName);
         when(entityManager.merge(wydzial)).thenReturn(wydzial);
-    
+
         wydzialDao.update(wydzial);
-    
+
         verify(entityManager, times(1)).merge(wydzial);
 
         assertNotNull(wydzial.getId());
@@ -68,12 +68,12 @@ class WydzialDaoImplTest {
     }
 
     @Test
-    void testDelete() {    
+    void shouldRemoveEntity_WhenWydzialExistsInPersistenceContext() {
         when(entityManager.contains(wydzial)).thenReturn(true);
-    
+
         wydzialDao.delete(wydzial);
-    
+
         verify(entityManager, times(1)).remove(wydzial);
     }
-    
+
 }
